5fedee642f6cda8b16d18cd384843de23f7b02b3
samsoncsv <samson.svendsen@vipps.no>
2025-10-02 18:34:54 +0200

    adding deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://samsoncs.github.io/matbutikkens-hemmeligheter/",
+  base: "/matbutikkens-hemmeligheter/",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });

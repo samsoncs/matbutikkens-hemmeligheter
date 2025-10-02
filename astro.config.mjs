9f276a91436dd85e8133961c8d716a253d2160a8
samsoncs <samsoncsv@gmail.com>
2025-10-02 20:04:08 +0200

    Adding prettier (#3)
    * prettify
    
    * removing negative z in pics
    
    * adding
    
    ---------
    
    Co-authored-by: samsoncsv <samson.svendsen@vipps.no>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
+const isCI = process.env.GITHUB_ACTIONS === "true";
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   site: "https://samsoncs.github.io/matbutikkens-hemmeligheter/",
-  base: "/matbutikkens-hemmeligheter/",
+  base: isCI || isProd ? "/matbutikkens-hemmeligheter/" : undefined,
   vite: {
     plugins: [tailwindcss()],
   },
